18d59f298d0bd8f5196b2263c32653b52fdb70e2
Bryan Ordoñez <201404243@ingenieria.usac.edu.gt>
2019-11-13 17:16:30 -0600

    update

--- src/components/my-view1.js
+++ src/components/my-view1.js
@@ -61,6 +61,8 @@ class MyView1 extends PageViewElement {
         width: 100%;
         height: 70%;
         border: 1px solid #d6d0ce;
+        margin-top: 0 !important;
+        margin-bottom: 0 !important;
       }
 
       .titulomensaje{
@@ -68,6 +70,8 @@ class MyView1 extends PageViewElement {
         width: 100%;
         height: 30%;
         border: 1px solid #d6d0ce;
+        margin-top: 0 !important;
+        margin-bottom: 0 !important;
       }
 
       .usuariop{
